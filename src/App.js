cec9401b18918b534abfa0fd12632542196b5416
Angela Xu <angela.xu.dev@gmail.com>
2023-02-14 12:28:27 -0500

    created blank page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello everyone!</h1>
+      <Navbar />
     </div>
   );
 }
