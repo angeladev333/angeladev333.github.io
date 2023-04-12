00db1fbf3a33c561742d608874f8fa5248b074d8
angelaxudev333 <angela.xu.dev@gmail.com>
2023-04-12 01:27:29 -0400

    spline image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,24 +6,28 @@ import './Hero.css';
 function Hero() {
   return (
     <div className="hero-container">
-      <video src="/public/elements/videos/video-2.mp4" autoPlay loop muted />
+      
+      <iframe title="Angela Working" src='https://my.spline.design/roomgirlworkingcopy-e351c5c694eb3e3745e84590c9a661e3/' frameborder='0' width='100%' height='100%'></iframe>
       <h1>Hi, my name is Angela Skie Xu.</h1>
       <p>I am a Computer Science student at University of Waterloo</p>
       <div className="hero-btns">
         <Button
           className="btns"
-          buttonStyle="btn--outline"
+          buttonStyle="btn--primary"
           buttonSize="btn--large"
+          onClick={console.log('hey')}
         >
-          GET STARTED
+          RESUME <i className="fas fa-arrow-circle-down"></i>
+
         </Button>
         <Button
           className="btns"
-          buttonStyle="btn--primary"
+          buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          WATCH TRAILER <i className='far fa-play-circle' />
+          PROJECTS
         </Button>
+        
       </div>
     </div>
   );
