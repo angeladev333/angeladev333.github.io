8cc22a8aec5cfafa560e707e59f300bf007ad08b
angelaxudev333 <angela.xu.dev@gmail.com>
2023-06-21 23:59:22 -0400

    update z-indexes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,36 @@
 import React from "react";
-import '../App.css';
-import {Button} from './Button';
-import './Hero.css';
+import "../App.css";
+import { Button } from "./Button";
+import "./Hero.css";
 
 function Hero() {
   return (
     <div className="hero-container">
-      
-      {/* <iframe title="Angela Working" src='https://my.spline.design/roomgirlworkingcopy-e351c5c694eb3e3745e84590c9a661e3/' frameborder='0' width='100%' height='100%'></iframe> */}
-      <iframe title="Angela Working" src='https://my.spline.design/roomgirlworkingcopy-e351c5c694eb3e3745e84590c9a661e3/' frameborder='0' width='100%' height='100%'></iframe>
-      <h1>Hey, I'm<br/> 
-      <span className="angela-name">Angela Skie Xu.</span></h1>
-      <p>I am a Computer Science student<br/> at University of Waterloo</p>
+      <iframe
+        title="Angela Working"
+        src="https://my.spline.design/roomgirlworkingcopy-e351c5c694eb3e3745e84590c9a661e3/"
+        frameborder="0"
+        width="100%"
+        height="100%"
+      ></iframe>
+
+      <h1>
+        Hey, I'm
+        <br />
+        <span className="angela-name">Angela Skie Xu.</span>
+      </h1>
+      <p>
+        I am a Computer Science student
+        <br /> at University of Waterloo
+      </p>
       <div className="hero-btns">
         <Button
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
-          onClick={console.log('hey')}
+          onClick={console.log("hey")}
         >
           RESUME <i className="fas fa-arrow-circle-down"></i>
-
         </Button>
         <Button
           className="btns"
@@ -29,10 +39,8 @@ function Hero() {
         >
           PROJECTS
         </Button>
-        
       </div>
-      
-      </div>
+    </div>
   );
 }
 
