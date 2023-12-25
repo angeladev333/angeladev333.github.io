04470874bc7b07ac5cfbaaf04fa3a07489473ca5
angelaxudev333 <angela.xu.dev@gmail.com>
2023-12-24 21:22:30 -0500

    fix? help broken?

--- client/src/components/pages/AboutMe.js
+++ client/src/components/pages/AboutMe.js
@@ -7,7 +7,7 @@ export default function AboutMe(props) {
       <div id={props.id} className="text-center">
       <h1 className="text-center text-4xl text-secondary lg:text-5xl">ABOUT ME</h1>
         <div className="bg-white mt-10 p-6 rounded-lg shadow-lg mx-auto max-w-6xl" style={{ backgroundColor: '#bd8ea4' }}> 
-        <div className="bg-white p-2 rounded-lg shadow-lg mx-auto max-w-6xl" style={{ backgroundColor: 'white' }}> 
+        <div className="bg-white p-1 rounded-lg shadow-lg mx-auto max-w-6xl" style={{ backgroundColor: 'white' }}> 
           <div className="flex flex-wrap justify-center items-center">
             <p className="text-left w-3/5 mx-4 mt-4 text-secondary text-lg font-body font-medium">
               💻 2nd Year CS Honours @ UWaterloo CGPA: 3.9/4; AI Specilization; Computational Mathematics Minor; Scotiabank Women in CS Scholarship ($10k) <br /><br />
